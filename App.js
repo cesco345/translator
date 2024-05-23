2c0c3260f2f20148346a279ef9d71e477a7b607f
Francesco Piscani <fpiscani@gmail.com>
2024-05-22 20:29:18 -0400

    use map and props

--- App.js
+++ App.js
@@ -2,18 +2,20 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import styles from "./App_styles";
 import ImageOption from "./src/components/ImageOption";
+import question from "./assets/data/oneQuestionWithOption";
 
 const App = () => {
-  const status = "ok";
-
   return (
     <View style={styles.root}>
-      <Text style={styles.title}>Which of these is the glass?</Text>
+      <Text style={styles.title}>{question.question}</Text>
       <View style={styles.optionsContainer}>
-        <ImageOption />
-        <ImageOption />
-        <ImageOption />
-        <ImageOption />
+        {question.options.map((option) => (
+          <ImageOption
+            key={option.text}
+            image={option.image}
+            text={option.text}
+          />
+        ))}
       </View>
     </View>
   );
--- src/components/ImageOption/ImageOption.js
+++ src/components/ImageOption/ImageOption.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 import styles from "./styles";
-const ImageOption = () => {
+
+const ImageOption = ({ image, text }) => {
   return (
     <View style={styles.optionContainer}>
       <Image
         source={{
-          uri: "https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/cup.png",
+          uri: image,
         }}
         resizeMode="contain"
         style={styles.optionImage}
         onLoad={() => console.log("Glass image loaded successfully")}
         onError={(error) => console.log("Error loading glass image", error)}
       />
-      <Text style={styles.optionText}>Cup</Text>
+      <Text style={styles.optionText}>{text}</Text>
     </View>
   );
 };
